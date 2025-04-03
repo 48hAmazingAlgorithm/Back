3c25cfde9ee88e07c2d7cee31b78c36c66af4281
theo.delporte <theo.delporte@ynov.com>
2025-04-03 11:30:58 +0200

    dseb

--- server.go
+++ server.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	router := gin.Default();
+	router := gin.Default()
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message" : "bienvenue dans l'api",
+			"message": "bienvenue dans l'api",
 		})
 	})
 	router.Run(":8080")
 }
-
